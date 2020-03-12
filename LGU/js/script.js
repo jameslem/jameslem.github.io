157e9abaf6e43c8d36d35d7c3f7ff1d3e8fb4cc0
James Lem <jamesleo_cute@yahoo.com>
2020-03-12 13:55:22 +0800

    increase and decrease button added

--- LGU/js/script.js
+++ LGU/js/script.js
@@ -18,3 +18,28 @@ $('#nextBtn').click(function(){
         $('.slider').slider('pause');
     },800);
 });
+
+
+
+$( "#bigBtn" ).click(function() {
+  $("#smallBtn").removeClass( "visibility" );
+  $( "#bigBtn").addClass( "visibility" );
+    $("#smallBtn").show();
+    $( "#bigBtn").hide();
+
+    // $("#font_body").addClass( "big-font" );
+    // $(".row").addClass( "big-font" );
+    $("nav ul li a").addClass( "big-font" );
+     $("p").addClass( "big-font" );
+
+});
+
+$( "#smallBtn" ).click(function() {
+  $("#bigBtn").removeClass( "visibility" );
+  $( "#smallBtn").addClass( "visibility" );
+    $("#bigBtn").show();
+
+    // $(".row").removeClass( "big-font" );
+    $("nav ul li a").removeClass( "big-font" );
+    $("p").removeClass( "big-font" );
+});
